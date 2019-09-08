7a265976345fb1b184e4ae6e5a8e868fd6759f22
Aryan Raj <contact@aryanraj.com>
2019-09-08 08:53:08 +0530

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Social Profiles
+ * Plugin URI:  https://github.com/rockingaryan/social-profiles
+ * Description: A lightweight & fastest social profiles plugin.
+ * Author:      Aryan Raj
+ * Author URI:  https://www.aryanraj.com/
+ * Version:     1.0.0
+ * License:     GPL-2.0+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package Social Profiles
+ */
+ 
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
