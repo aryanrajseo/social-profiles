a76b2b33baf06fbf1fae1dfd444659878c45fe41
Aryan Ra <contact@aryanraj.com>
2019-09-09 13:46:48 +0530

    updated shortcode

--- plugin.php
+++ plugin.php
@@ -9,7 +9,7 @@
  * License:     GPL-2.0+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
- * @package Social Profiles
+ * @package Social_Profiles
  */
  
 // If this file is called directly, abort.
@@ -18,18 +18,18 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 // Enquque Social Media Profiles Styles.
-add_action( 'wp_enqueue_scripts', 'social_media_profiles_styles' );
-function social_media_profiles_styles() {
-	wp_enqueue_style( 'social-media_profiles', plugin_dir_url( __FILE__ ) .  'style.css', array(), '1.0.0', 'all'  );
+add_action( 'wp_enqueue_scripts', 'social_profiles_styles' );
+function social_profiles_styles() {
+	wp_enqueue_style( 'social-profiles', plugin_dir_url( __FILE__ ) .  'style.css', array(), '1.0.0', 'all'  );
 }
 
 // Create a Social Media Profiles Shortcode
-add_shortcode( 'social-media', 'custom_social_media_profiles_shortcode' );
+add_shortcode( 'social-profiles', 'custom_social_profiles_shortcode' );
 
-function custom_social_media_profiles_shortcode( $atts, $content = null ) {
+function custom_social_profiles_shortcode( $atts, $content = null ) {
 
 	// Attributes
-	$smp_atts = shortcode_atts(
+	$sp_atts = shortcode_atts(
 		array(
 
 			// Content unqieue ID and link setting
@@ -69,16 +69,16 @@ function custom_social_media_profiles_shortcode( $atts, $content = null ) {
 	
 	
 	// Construct output of the social media profiles
-	$content .= '<!-- Social Profiles --><div id="social-media-profiles-'.$smp_atts['id'].'" class="social-media-profiles"> <ul class="align'.$smp_atts['align'].'">';
+	$content .= '<!-- Social Profiles --><div id="social-profiles-'.$sp_atts['id'].'" class="social-profiles"> <ul class="align'.$sp_atts['align'].'">';
 	
 
 	// facebook
-	if ( ! empty( $smp_atts['flink'] ) ) {
+	if ( ! empty( $sp_atts['flink'] ) ) {
 		$content .= '
-		<li class="smp-facebook">
-			<a href="'.$smp_atts['flink'].'" target="'.$smp_atts['target'].'" rel="'.$smp_atts['rel'].'">
-				<svg role="img" class="smp-img-facebook" aria-labelledby="smp-facebook">
-					<title id="smp-facebook">Facebook</title>
+		<li class="sp-facebook">
+			<a href="'.$sp_atts['flink'].'" target="'.$sp_atts['target'].'" rel="'.$sp_atts['rel'].'">
+				<svg role="img" class="sp-img-facebook" aria-labelledby="sp-facebook">
+					<title id="sp-facebook">Facebook</title>
 					<use href="'.$svgPath.'#social-facebook" xlink:href="'.$svgPath.'#social-facebook"></use>
 				</svg>
 			</a>
@@ -86,12 +86,12 @@ function custom_social_media_profiles_shortcode( $atts, $content = null ) {
 	}
 
 	// twitter
-	if ( ! empty( $smp_atts['tlink'] ) ) {
+	if ( ! empty( $sp_atts['tlink'] ) ) {
 		$content .= '
-		<li class="smp-twitter">
-			<a href="'.$smp_atts['tlink'].'" target="'.$smp_atts['target'].'" rel="'.$smp_atts['rel'].'">
-				<svg role="img" class="smp-img-twitter" aria-labelledby="smp-twitter">
-					<title id="smp-twitter">Twitter</title>
+		<li class="sp-twitter">
+			<a href="'.$sp_atts['tlink'].'" target="'.$sp_atts['target'].'" rel="'.$sp_atts['rel'].'">
+				<svg role="img" class="sp-img-twitter" aria-labelledby="sp-twitter">
+					<title id="sp-twitter">Twitter</title>
 					<use href="'.$svgPath.'#social-twitter" xlink:href="'.$svgPath.'#social-twitter"></use>
 				</svg>
 			</a>
@@ -99,12 +99,12 @@ function custom_social_media_profiles_shortcode( $atts, $content = null ) {
 	}
 
 	// instagram
-	if ( ! empty( $smp_atts['ilink'] ) ) {
+	if ( ! empty( $sp_atts['ilink'] ) ) {
 		$content .= '
-		<li class="smp-instagram">
-			<a href="'.$smp_atts['ilink'].'" target="'.$smp_atts['target'].'" rel="'.$smp_atts['rel'].'">
-				<svg role="img" class="smp-img-instagram" aria-labelledby="smp-instagram">
-					<title id="smp-instagram">Instgram</title>
+		<li class="sp-instagram">
+			<a href="'.$sp_atts['ilink'].'" target="'.$sp_atts['target'].'" rel="'.$sp_atts['rel'].'">
+				<svg role="img" class="sp-img-instagram" aria-labelledby="sp-instagram">
+					<title id="sp-instagram">Instgram</title>
 					<use href="'.$svgPath.'#social-instagram" xlink:href="'.$svgPath.'#social-instagram"></use>
 				</svg>
 			</a>
@@ -112,12 +112,12 @@ function custom_social_media_profiles_shortcode( $atts, $content = null ) {
 	}
 
 	// pinterest
-	if ( ! empty( $smp_atts['plink'] ) ) {
+	if ( ! empty( $sp_atts['plink'] ) ) {
 		$content .= '
-		<li class="smp-pinterest">
-			<a href="'.$smp_atts['plink'].'" target="'.$smp_atts['target'].'" rel="'.$smp_atts['rel'].'">
-				<svg role="img" class="smp-img-pinterest" aria-labelledby="smp-pinterest">
-					<title id="smp-pinterest">Pinterest</title>
+		<li class="sp-pinterest">
+			<a href="'.$sp_atts['plink'].'" target="'.$sp_atts['target'].'" rel="'.$sp_atts['rel'].'">
+				<svg role="img" class="sp-img-pinterest" aria-labelledby="sp-pinterest">
+					<title id="sp-pinterest">Pinterest</title>
 					<use href="'.$svgPath.'#social-pinterest" xlink:href="'.$svgPath.'#social-pinterest"></use>
 				</svg>
 			</a>
@@ -125,12 +125,12 @@ function custom_social_media_profiles_shortcode( $atts, $content = null ) {
 	}
 
 	// youtube
-	if ( ! empty( $smp_atts['ylink'] ) ) {
+	if ( ! empty( $sp_atts['ylink'] ) ) {
 		$content .= '
-		<li class="smp-youtube">
-			<a href="'.$smp_atts['ylink'].'" target="'.$smp_atts['target'].'" rel="'.$smp_atts['rel'].'">
-				<svg role="img" class="smp-img-youtube" aria-labelledby="smp-youtube">
-					<title id="smp-youtube">Youtube</title>
+		<li class="sp-youtube">
+			<a href="'.$sp_atts['ylink'].'" target="'.$sp_atts['target'].'" rel="'.$sp_atts['rel'].'">
+				<svg role="img" class="sp-img-youtube" aria-labelledby="sp-youtube">
+					<title id="sp-youtube">Youtube</title>
 					<use href="'.$svgPath.'#social-youtube" xlink:href="'.$svgPath.'#social-youtube"></use>
 				</svg>
 			</a>
@@ -138,12 +138,12 @@ function custom_social_media_profiles_shortcode( $atts, $content = null ) {
 	}
 
 	// rss
-	if ( ! empty( $smp_atts['rlink'] ) ) {
+	if ( ! empty( $sp_atts['rlink'] ) ) {
 		$content .= '
-		<li class="smp-rss">
-			<a href="'.$smp_atts['rlink'].'" target="'.$smp_atts['target'].'" rel="'.$smp_atts['rel'].'">
-				<svg role="img" class="smp-img-rss" aria-labelledby="smp-rss">
-					<title id="smp-rss">RSS</title>
+		<li class="sp-rss">
+			<a href="'.$sp_atts['rlink'].'" target="'.$sp_atts['target'].'" rel="'.$sp_atts['rel'].'">
+				<svg role="img" class="sp-img-rss" aria-labelledby="sp-rss">
+					<title id="sp-rss">RSS</title>
 					<use href="'.$svgPath.'#social-rss" xlink:href="'.$svgPath.'#social-rss"></use>
 				</svg>
 			</a>
@@ -156,22 +156,22 @@ function custom_social_media_profiles_shortcode( $atts, $content = null ) {
 	$content .= '<style type="text/css" media="screen">';
 	
 	$content .= '
-	#social-media-profiles-'.$smp_atts['id'].' ul li a,
-	#social-media-profiles-'.$smp_atts['id'].' ul li a:hover,
-	#social-media-profiles-'.$smp_atts['id'].' ul li a:focus {
-		background-color: '.$smp_atts['background-color'].' !important;
-		color: '.$smp_atts['color'].' !important;
-  		border-radius: '.$smp_atts['border-radius'].';
-  		border: '.$smp_atts['border'].' solid !important; 
-  		font-size: calc('.$smp_atts['font-size'].' / 2);
-  		padding: '.$smp_atts['padding'].';
+	#social-profiles-'.$sp_atts['id'].' ul li a,
+	#social-profiles-'.$sp_atts['id'].' ul li a:hover,
+	#social-profiles-'.$sp_atts['id'].' ul li a:focus {
+		background-color: '.$sp_atts['background-color'].' !important;
+		color: '.$sp_atts['color'].' !important;
+  		border-radius: '.$sp_atts['border-radius'].';
+  		border: '.$sp_atts['border'].' solid !important; 
+  		font-size: calc('.$sp_atts['font-size'].' / 2);
+  		padding: '.$sp_atts['padding'].';
 	}
 
-	#social-media-profiles-'.$smp_atts['id'].' ul li a:hover,
-	#social-media-profiles-'.$smp_atts['id'].' ul li a:focus {
-  		background-color: '.$smp_atts['hf-background-color'].' !important;
-		color: '.$smp_atts['hf-color'].' !important;
-		border-color: '.$smp_atts['hf-border-color'].' !important;
+	#social-profiles-'.$sp_atts['id'].' ul li a:hover,
+	#social-profiles-'.$sp_atts['id'].' ul li a:focus {
+  		background-color: '.$sp_atts['hf-background-color'].' !important;
+		color: '.$sp_atts['hf-color'].' !important;
+		border-color: '.$sp_atts['hf-border-color'].' !important;
 	}
 	';
 
